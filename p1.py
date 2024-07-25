8fb39d2f6dfc9f3bbf4f6ecdff9a310cf4f5586d
priyareddyvanga25 <priyareddyvanga2004@gmail.com>
2024-07-25 14:26:50 +0530

    Your commit message

---
+++ p1.py
@@ -0,0 +1,13 @@
+def main():
+    # Input two numbers from the user
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    # Calculate the sum
+    sum = num1 + num2
+    
+    # Print the sum
+    print(f"The sum of {num1} and {num2} is: {sum}")
+
+if __name__ == "__main__":
+    main()
